0eb346c5b8e6698192cccb60554378540770c554
YarethLeal <yarethleal65@gmail.com>
2024-07-19 20:40:24 -0600

    Add Islas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://yarethleal-astro-blog.netlify.app/"
+  site: "https://yarethleal-astro-blog.netlify.app/",
+  integrations: [react()]
 });
